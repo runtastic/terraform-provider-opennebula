a0a3d2ccab171f47f86ab44f5cefbee0dc0ae106
Niko Dominkowitsch <niko.dominkowitsch@runtastic.com>
2017-07-03 15:06:00 +0200

    main.go: change path to opennebula provider

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/akurz/terraform-provider-opennebula/opennebula"
 	"github.com/hashicorp/terraform/plugin"
+	"stash.runtastic.com/ropscode/opennebula-provider/opennebula"
 )
 
 func main() {
