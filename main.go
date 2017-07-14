615a12874fce04664973d6ee53abe79aacc41aee
Eric Ripa <eripa@demonware.net>
2017-07-14 10:07:32 -0700

    Update import statement in main.go to match project rename

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/larribas/terraform-provider-opennebula/opennebula"
+	"github.com/runtastic/terraform-provider-opennebula/opennebula"
 )
 
 func main() {
